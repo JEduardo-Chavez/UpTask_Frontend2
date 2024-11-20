621cad2aac6e070b02043881ce855378db4082b7
educhavez <echavez@gruporivero.com>
2024-11-20 16:51:53 -0600

    Prueba de actualizacion

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -2,7 +2,7 @@ import {isAxiosError} from "axios";
 import api from "@/lib/axios";
 import { CheckPasswordForm, ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm, userSchema } from "../types";
 
-
+// UN COMENTARIO
 export async function createAccount(formData : UserRegistrationForm) {
     try {
         const url = 'auth/create-account'
